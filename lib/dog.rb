5b6b8f956687baf4da7c5b3ec98d27da69c1e911
Christopher Beltran <christod.beltran@gmail.com>
2018-10-27 07:50:09 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -70,7 +70,11 @@ def self.create(name:, breed:)
   new_dog
 end 
 
-
+def self.find_by_id(id)
+    sql = "SELECT * FROM songs WHERE id = ?"
+    result = DB[:conn].execute(sql, id)[0]
+    Dog.new(result[0], result[1], result[2])
+  end
 
   
 end 
